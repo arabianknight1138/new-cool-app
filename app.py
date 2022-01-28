75c86c54c1a520c60313fa2768fe95ce47a706a4
Afra Jamion <afrajamion@Afras-MacBook-Air.local>
2022-01-28 15:12:28 +0000

    adding afra's favourite food

--- app.py
+++ app.py
@@ -6,10 +6,12 @@ app = Flask(__name__)
 # Add your name in this list!
 trainees = [
     "Harry",
+    "Afra"
 ]
 # Add a food you like (or don't!) in this list!
 foods = [
     "pizza",
+    "teriyaki chicken"
 ]
 
 @app.route('/')
